4f0e1d6673c23feeb7d1eb7c7d1e266021587086
Dhruv Bindra <dbindra@asu.edu>
2024-11-07 11:58:10 -0700

    implement graceful shutdown
    Signed-off-by: Dhruv Bindra <dbindra@asu.edu>

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/dhruv-assessment/api-server/handler"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
@@ -14,5 +22,26 @@ func main() {
 
 	go handler.WaitForSQSResponseMessageTest()
 
-	e.Logger.Fatal(e.Start(":1323"))
+	// e.Logger.Fatal(e.Start(":1323"))
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Graceful shutdown handling
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit // Wait for a termination signal
+
+	log.Println("Shutting down gracefully...")
+
+	// Context with timeout for graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 360*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
